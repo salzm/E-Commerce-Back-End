c1d9464d8e1b17c1d1696d015a421f7b77a01246
salzm <salmonzoor@gmail.com>
2023-05-11 09:35:20 -0400

    added the columns for the Producttag js file

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,37 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      unique: true,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      refrences: {
+        model: "product",
+        key: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
